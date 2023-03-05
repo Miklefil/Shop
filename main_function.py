027ad4101aeb22d6fc6dd46e2d427ed66309c98b
twist <twistysmig@mail.ru>
2023-03-05 19:39:15 +0300

    in prossess added tests and main function

--- main_function.py
+++ main_function.py
@@ -1,29 +1,11 @@
-class Item:
-    pay_rate = 0.85
-    all = []
-    def __init__(self, name, price, quantity ):
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        self.all.append(self)
+from product_statistic import*
 
+Item.instantiate_from_csv('items.csv')  # создание объектов из данных файла
+print(len(Item.all_items))  # в файле 5 записей с данными по товарам
 
-    def calculate_total_price(self):
-        self.price1 = self.price * self.quantity
-        return self.price1
+item1 = Item.all_items[0]
+print(item1.item_name)
 
-
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
-
-Item.pay_rate = 0.8  # устанавливаем новый уровень цен
-item1.apply_discount()
-print(item1.price)
-print(item2.price)
-
-print(Item.all)
+print(Item.is_integer(5))
+print(Item.is_integer(5.0))
+print(Item.is_integer(5.5))
